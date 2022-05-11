2f7ebe74562ef9201bb2c273bbe02ff2c8aa8b2b
Evan Lemieux <elemieux@desire2learn.com>
2022-05-11 15:31:08 -0500

    Update winston to latest version

--- src/log.js
+++ src/log.js
@@ -1,13 +1,11 @@
 'use strict';
-
 const winston = require('winston');
 
-const logger = new (winston.Logger)({
+const logger = winston.createLogger({
+	level: process.env.LOG_LEVEL || 'info',
 	transports: [
-		new (winston.transports.Console)({ timestamp: true })
+		new winston.transports.Console({ timestamp: true })
 	]
 });
-
-logger.level = process.env.LOG_LEVEL || 'info';
 
 module.exports = logger;
