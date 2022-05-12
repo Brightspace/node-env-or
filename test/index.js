42bdede561dc454baf18c0d65c45cd88358c5728
Owen Smith <owen@omsmith.ca>
2022-05-12 09:38:07 -0400

    replace winston with an EventEmitter

--- test/index.js
+++ test/index.js
@@ -7,14 +7,12 @@ const
 	expect = chai.expect,
 	sinon = require('sinon');
 
-const
-	envOr = require('../'),
-	log = require('../src/log');
+const envOr = require('../');
 
 chai.use(require('sinon-chai'));
 
 describe('envOr', function() {
-	let sandbox;
+	let listener;
 
 	function setAs(val) {
 		if (val !== undefined) {
@@ -33,125 +31,212 @@ describe('envOr', function() {
 	};
 
 	beforeEach(function() {
-		sandbox = sinon.createSandbox();
-		sandbox.stub(log, 'error');
-		sandbox.stub(log, 'info');
-		sandbox.stub(log, 'warn');
+		listener = sinon.stub();
+		envOr.on('access', listener);
 	});
 
 	afterEach(function() {
-		sandbox.restore();
-		setAs(undefined);
+		envOr.removeListener('access', listener);
 	});
 
 	describe('Accessing defined env', function() {
 		describe('with a numeric default', function() {
-			it('should give the value as number if it is parsable, and info log', function() {
+			it('should give the value as number if it is parsable, and raise event', function() {
 				setAs('456');
 				expect(get(1)).to.be.a('number').and.to.equal(456);
-				expect(log.info).to.have.been.calledWithMatch('"456" (number)');
-				log.info.reset();
+				expect(listener).to.have.been.calledWithMatch({
+					name: 'ENV_TEST_VAR',
+					value: 456,
+					present: true,
+					required: false
+				});
+				listener.reset();
 
 				setAs('-456');
 				expect(get(1)).to.be.a('number').and.to.equal(-456);
-				expect(log.info).to.have.been.calledWithMatch('"-456" (number)');
-				log.info.reset();
+				expect(listener).to.have.been.calledWithMatch({
+					name: 'ENV_TEST_VAR',
+					value: -456,
+					present: true,
+					required: false
+				});
+				listener.reset();
 
 				setAs('1.5');
 				expect(get(1)).to.be.a('number').and.to.equal(1.5);
-				expect(log.info).to.have.been.calledWithMatch('"1.5" (number)');
-				log.info.reset();
+				expect(listener).to.have.been.calledWithMatch({
+					name: 'ENV_TEST_VAR',
+					value: 1.5,
+					present: true,
+					required: false
+				});
+				listener.reset();
 			});
 
-			it('should give the default number if not parsable, and warn', function() {
+			it('should give the default number if not parsable, and raise event', function() {
 				setAs('foozles');
 				expect(get(1)).to.be.a('number').and.to.equal(1);
-				expect(log.warn).to.have.been.calledWithMatch('"1" (number)');
-				log.warn.reset();
+				expect(listener).to.have.been.calledWithMatch({
+					name: 'ENV_TEST_VAR',
+					value: 1,
+					present: false,
+					required: false
+				});
+				listener.reset();
 
 				setAs('0/0');
 				expect(get(1)).to.be.a('number').and.to.equal(1);
-				expect(log.warn).to.have.been.calledWithMatch('"1" (number)');
-				log.warn.reset();
+				expect(listener).to.have.been.calledWithMatch({
+					name: 'ENV_TEST_VAR',
+					value: 1,
+					present: false,
+					required: false
+				});
+				listener.reset();
 			});
 		});
 
 		describe('with a boolean default', function() {
-			it('should give "true" as a boolean if it is truthy, and info log', function() {
+			it('should give "true" as a boolean if it is truthy, and raise event', function() {
 				setAs('true');
 				expect(get(false)).to.be.a('boolean').and.to.equal(true);
-				expect(log.info).to.have.been.calledWithMatch('"true" (boolean)');
-				log.info.reset();
+				expect(listener).to.have.been.calledWithMatch({
+					name: 'ENV_TEST_VAR',
+					value: true,
+					present: true,
+					required: false
+				});
+				listener.reset();
 
 				setAs('TRUE');
 				expect(get(false)).to.be.a('boolean').and.to.equal(true);
-				expect(log.info).to.have.been.calledWithMatch('"true" (boolean)');
-				log.info.reset();
+				expect(listener).to.have.been.calledWithMatch({
+					name: 'ENV_TEST_VAR',
+					value: true,
+					present: true,
+					required: false
+				});
+				listener.reset();
 
 				setAs('1');
 				expect(get(false)).to.be.a('boolean').and.to.equal(true);
-				expect(log.info).to.have.been.calledWithMatch('"true" (boolean)');
-				log.info.reset();
+				expect(listener).to.have.been.calledWithMatch({
+					name: 'ENV_TEST_VAR',
+					value: true,
+					present: true,
+					required: false
+				});
+				listener.reset();
 			});
 
-			it('should give "false" as a boolean if it is not truthy, and info log', function() {
+			it('should give "false" as a boolean if it is not truthy, and raise event', function() {
 				setAs('false');
 				expect(get(true)).to.be.a('boolean').and.to.equal(false);
-				expect(log.info).to.have.been.calledWithMatch('"false" (boolean)');
-				log.info.reset();
+				expect(listener).to.have.been.calledWithMatch({
+					name: 'ENV_TEST_VAR',
+					value: false,
+					present: true,
+					required: false
+				});
+				listener.reset();
 
 				setAs('FALSE');
 				expect(get(true)).to.be.a('boolean').and.to.equal(false);
-				expect(log.info).to.have.been.calledWithMatch('"false" (boolean)');
-				log.info.reset();
+				expect(listener).to.have.been.calledWithMatch({
+					name: 'ENV_TEST_VAR',
+					value: false,
+					present: true,
+					required: false
+				});
+				listener.reset();
 
 				setAs('0');
 				expect(get(true)).to.be.a('boolean').and.to.equal(false);
-				expect(log.info).to.have.been.calledWithMatch('"false" (boolean)');
-				log.info.reset();
+				expect(listener).to.have.been.calledWithMatch({
+					name: 'ENV_TEST_VAR',
+					value: false,
+					present: true,
+					required: false
+				});
+				listener.reset();
 
 				setAs('hi everyone!');
 				expect(get(true)).to.be.a('boolean').and.to.equal(false);
-				expect(log.info).to.have.been.calledWithMatch('"false" (boolean)');
-				log.info.reset();
+				expect(listener).to.have.been.calledWithMatch({
+					name: 'ENV_TEST_VAR',
+					value: false,
+					present: true,
+					required: false
+				});
+				listener.reset();
 			});
 		});
 
 		describe('with a string default', function() {
-			it('should give value as a string, and info log', function() {
+			it('should give value as a string, and raise event', function() {
 				setAs('true');
 				expect(get('foo')).to.be.a('string').and.to.equal('true');
-				expect(log.info).to.have.been.calledWithMatch('"true" (string)');
-				log.info.reset();
+				expect(listener).to.have.been.calledWithMatch({
+					name: 'ENV_TEST_VAR',
+					value: 'true',
+					present: true,
+					required: false
+				});
+				listener.reset();
 
 				setAs('6');
 				expect(get('foo')).to.be.a('string').and.to.equal('6');
-				expect(log.info).to.have.been.calledWithMatch('"6" (string)');
-				log.info.reset();
+				expect(listener).to.have.been.calledWithMatch({
+					name: 'ENV_TEST_VAR',
+					value: '6',
+					present: true,
+					required: false
+				});
+				listener.reset();
 
 				setAs('hi everyone!');
 				expect(get('foo')).to.be.a('string').and.to.equal('hi everyone!');
-				expect(log.info).to.have.been.calledWithMatch('"hi everyone!" (string)');
-				log.info.reset();
+				expect(listener).to.have.been.calledWithMatch({
+					name: 'ENV_TEST_VAR',
+					value: 'hi everyone!',
+					present: true,
+					required: false
+				});
+				listener.reset();
 			});
 		});
 
 		describe('with no default', function() {
-			it('should give value as a string, and info log', function() {
+			it('should give value as a string, and raise event', function() {
 				setAs('true');
 				expect(get()).to.be.a('string').and.to.equal('true');
-				expect(log.info).to.have.been.calledWithMatch('"true" (string)');
-				log.info.reset();
+				expect(listener).to.have.been.calledWithMatch({
+					name: 'ENV_TEST_VAR',
+					value: 'true',
+					present: true,
+					required: false
+				});
+				listener.reset();
 
 				setAs('6');
 				expect(get()).to.be.a('string').and.to.equal('6');
-				expect(log.info).to.have.been.calledWithMatch('"6" (string)');
-				log.info.reset();
+				expect(listener).to.have.been.calledWithMatch({
+					name: 'ENV_TEST_VAR',
+					value: '6',
+					present: true,
+					required: false
+				});
+				listener.reset();
 
 				setAs('hi everyone!');
 				expect(get()).to.be.a('string').and.to.equal('hi everyone!');
-				expect(log.info).to.have.been.calledWithMatch('"hi everyone!" (string)');
-				log.info.reset();
+				expect(listener).to.have.been.calledWithMatch({
+					name: 'ENV_TEST_VAR',
+					value: 'hi everyone!',
+					present: true,
+					required: false
+				});
+				listener.reset();
 			});
 		});
 	});
@@ -173,65 +258,105 @@ describe('envOr', function() {
 				it('should exit the process', function() {
 					setAs(undefined);
 					expect(get.requireInProd.bind(undefined, or)).to.throw(/Fallbacks are not allowed/);
-					expect(log.error).to.have.been.calledWithMatch('Fallbacks are not allowed');
+					expect(listener).to.have.been.calledWithMatch({
+						name: 'ENV_TEST_VAR',
+						value: undefined,
+						present: false,
+						required: true
+					});
 				});
 			});
 		}
 
 		describe('with a numeric default', function() {
-			it('should give the default as anumber, and warn', function() {
+			it('should give the default as anumber, and raise event', function() {
 				setAs(undefined);
 				expect(get(456)).to.be.a('number').and.to.equal(456);
-				expect(log.warn).to.have.been.calledWithMatch('"456" (number)');
-				log.warn.reset();
+				expect(listener).to.have.been.calledWithMatch({
+					name: 'ENV_TEST_VAR',
+					value: 456,
+					present: false,
+					required: false
+				});
+				listener.reset();
 
 				setAs(undefined);
 				expect(get(-456)).to.be.a('number').and.to.equal(-456);
-				expect(log.warn).to.have.been.calledWithMatch('"-456" (number)');
-				log.warn.reset();
+				expect(listener).to.have.been.calledWithMatch({
+					name: 'ENV_TEST_VAR',
+					value: -456,
+					present: false,
+					required: false
+				});
+				listener.reset();
 
 				setAs(undefined);
 				expect(get(1.5)).to.be.a('number').and.to.equal(1.5);
-				expect(log.warn).to.have.been.calledWithMatch('"1.5" (number)');
-				log.warn.reset();
+				expect(listener).to.have.been.calledWithMatch({
+					name: 'ENV_TEST_VAR',
+					value: 1.5,
+					present: false,
+					required: false
+				});
+				listener.reset();
 			});
 
 			prodTest(456);
 		});
 
 		describe('with a boolean default', function() {
-			it('should give default as a boolean, and warn', function() {
+			it('should give default as a boolean, and raise event', function() {
 				setAs(undefined);
 				expect(get(true)).to.be.a('boolean').and.to.equal(true);
-				expect(log.warn).to.have.been.calledWithMatch('"true" (boolean)');
-				log.warn.reset();
+				expect(listener).to.have.been.calledWithMatch({
+					name: 'ENV_TEST_VAR',
+					value: true,
+					present: false,
+					required: false
+				});
+				listener.reset();
 
 				setAs(undefined);
 				expect(get(false)).to.be.a('boolean').and.to.equal(false);
-				expect(log.warn).to.have.been.calledWithMatch('"false" (boolean)');
-				log.warn.reset();
+				expect(listener).to.have.been.calledWithMatch({
+					name: 'ENV_TEST_VAR',
+					value: false,
+					present: false,
+					required: false
+				});
+				listener.reset();
 			});
 
 			prodTest(true);
 		});
 
 		describe('with a string default', function() {
-			it('should give default as a string, and warn', function() {
+			it('should give default as a string, and raise event', function() {
 				setAs(undefined);
 				expect(get('foo')).to.be.a('string').and.to.equal('foo');
-				expect(log.warn).to.have.been.calledWithMatch('"foo" (string)');
-				log.warn.reset();
+				expect(listener).to.have.been.calledWithMatch({
+					name: 'ENV_TEST_VAR',
+					value: 'foo',
+					present: false,
+					required: false
+				});
+				listener.reset();
 			});
 
 			prodTest('foo');
 		});
 
 		describe('with no default', function() {
-			it('should warn', function() {
+			it('should raise event', function() {
 				setAs(undefined);
 				expect(get()).to.equal(undefined);
-				expect(log.warn).to.have.been.calledWithMatch('No fallback provided');
-				log.warn.reset();
+				expect(listener).to.have.been.calledWithMatch({
+					name: 'ENV_TEST_VAR',
+					value: undefined,
+					present: false,
+					required: false
+				});
+				listener.reset();
 			});
 
 			prodTest(undefined);
